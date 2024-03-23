f85b1fd08b412521d41dc5d833f9e351cc46a688
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-03-24 08:57:16 +0900

    fix routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,29 @@
 class UserController < ApplicationController
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      render json: { status: 'User created successfully' }, status: :created
-    else
-      render json: { errors: user.errors.full_messages }, status: :bad_request
+  # def create
+  #   @user = User.new(user_params)
+  #   if @user.save
+  #     render json: { status: 'User created successfully' }, status: :created
+  #   else
+  #     render json: { errors: user.errors.full_messages }, status: :bad_request
+  #   end
+  # end
+
+  # private
+
+  #   def user_params
+  #     params.require(:user).permit(:email, :password, :password_confirmation)
+  #   end
+
+  def guest_login
+    User.find_or_create_by!(email: 'guest@example.com') do |new_user|
+      new_user.password = SecureRandom.urlsafe_base64
     end
+
+    token = encode_jwt_token_for(user)
+    render json: { token: token }
   end
 
-  private
-
-    def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation)
-    end
+  def encode_jwt_token_for(new_user)
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :todos, only: [:index, :create, :update, :destroy]
   resources :users, only: [:create]
+  post '/guest-login', to: 'users#guest_login'
 end
