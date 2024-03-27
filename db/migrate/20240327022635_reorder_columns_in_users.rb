9d10c626d671bb7eb1fe32b2dce90274adf8b5e0
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-03-27 11:28:10 +0900

    userstabelsのカラム順序変更

---
+++ db/migrate/20240327022635_reorder_columns_in_users.rb
@@ -0,0 +1,14 @@
+class ReorderColumnsInUsers < ActiveRecord::Migration[7.0]
+  def up
+    execute <<-SQL
+      ALTER TABLE users MODIFY COLUMN uid VARCHAR(255) AFTER id;
+    SQL
+    execute <<-SQL
+      ALTER TABLE users MODIFY COLUMN name VARCHAR(255) AFTER uid;
+    SQL
+    execute <<-SQL
+      ALTER TABLE users MODIFY COLUMN provider VARCHAR(255) AFTER name;
+    SQL
+    # created_at と updated_at は既に望む順序にあるため、再度指定は不要
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_24_124757) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_27_022635) do
   create_table "todos", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.boolean "completed"
@@ -19,12 +19,11 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_24_124757) do
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "provider"
     t.string "uid"
     t.string "name"
+    t.string "provider"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
