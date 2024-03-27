396b0a7262524bd6c75e7f84ab447f972387d67a
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-03-27 10:59:54 +0900

    guest_login コメントアウト

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -12,7 +12,7 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def destroy
-    user = User.find(params[:id])
+    user = User.find_by(uid: params[:uid])
     if user
       user.destroy
     else
@@ -22,19 +22,19 @@ class Api::V1::UsersController < ApplicationController
     render json: { error: e.message }, status: :internal_server_error
   end
 
-  def guest_login
-    User.find_or_create_by!(email: 'guest@example.com') do |user|
-      user.password = SecureRandom.urlsafe_base64
-    end
+  # def guest_login
+  #   User.find_or_create_by!(email: 'guest@example.com') do |user|
+  #     user.password = SecureRandom.urlsafe_base64
+  #   end
 
-    token = encode_jwt_token_for(user)
-    render json: { token: token }
-  end
+  #   token = encode_jwt_token_for(user)
+  #   render json: { token: token }
+  # end
 
-  def encode_jwt_token_for(user)
-    exp = 24.hours.from_now.to_i # 有効期限を24時間後に設定
-    payload = { user_id: user.id, exp: exp }
-    hmac_secret = ENV['JWT_SECRET_KEY']
-    JWT.encode(payload, hmac_secret, 'HS256')
-  end
+  # def encode_jwt_token_for(user)
+  #   exp = 24.hours.from_now.to_i # 有効期限を24時間後に設定
+  #   payload = { user_id: user.id, exp: exp }
+  #   hmac_secret = ENV['JWT_SECRET_KEY']
+  #   JWT.encode(payload, hmac_secret, 'HS256')
+  # end
 end
