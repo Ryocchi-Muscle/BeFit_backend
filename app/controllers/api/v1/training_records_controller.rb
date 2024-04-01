7006e44f09b691ab8c1e8c9e72733660004286c5
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-04-01 15:11:23 +0900

    create

--- app/controllers/api/v1/training_records_controller.rb
+++ app/controllers/api/v1/training_records_controller.rb
@@ -1,2 +1,38 @@
 class TrainingRecordsController < ApplicationController
+  # GET /todos
+  def index
+    @training_records = TrainingRecord.all
+    render json: @training_records
+  end
+
+  def create
+    @training_record = TrainingRecord.new(training_record_params)
+    if @training_record.save
+      render json: @training_record, status: :created, location: @training_record
+    else
+      render json: @training_record.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @todo.update(training_record_params)
+      render json: @training_record
+    else
+      render json: @training_record.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @training_record.destroy
+  end
+
+  private
+
+    def set_training_record
+      @training_record = TrainingRecord.find(params[:id])
+    end
+
+    def training_record_params
+      params.require(:training_record).permit(:title, :completed)
+    end
 end
