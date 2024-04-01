4a83e5c8055fa25684622c04561e473bce51a0b8
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-04-01 15:50:39 +0900

    fix

--- app/controllers/api/v1/training_records_controller.rb
+++ app/controllers/api/v1/training_records_controller.rb
@@ -1,8 +1,14 @@
 class TrainingRecordsController < ApplicationController
+  before_action :set_training_record, only: [:update, :destroy]
   # GET /todos
   def index
-    @training_records = TrainingRecord.all
-    render json: @training_records
+    @training_records = if params[:date]
+                          current_user.training_records.where(date: params[:date])
+                        else
+                          current_user.training_records
+                        end
+
+    render json: @training_records.includes(:training_sets)
   end
 
   def create
@@ -15,7 +21,7 @@ class TrainingRecordsController < ApplicationController
   end
 
   def update
-    if @todo.update(training_record_params)
+    if @training_record.update(training_record_params)
       render json: @training_record
     else
       render json: @training_record.errors, status: :unprocessable_entity
@@ -24,6 +30,7 @@ class TrainingRecordsController < ApplicationController
 
   def destroy
     @training_record.destroy
+    head :no_content #削除が成功した場合にはステータスコード204（No Content）を返す
   end
 
   private
@@ -33,6 +40,7 @@ class TrainingRecordsController < ApplicationController
     end
 
     def training_record_params
-      params.require(:training_record).permit(:title, :completed)
+      params.require(:training_record).permit(:date, :body_part, :menu,
+                                              training_sets_attributes: [:id, :weight, :reps, :completed, :_destroy])
     end
 end
