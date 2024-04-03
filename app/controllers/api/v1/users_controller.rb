4bcec130397265c84b36a3f82873c9e5bfffb9db
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-04-03 19:19:44 +0900

    delete guest_login

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -21,20 +21,4 @@ class Api::V1::UsersController < ApplicationController
   rescue StandardError => e
     render json: { error: e.message }, status: :internal_server_error
   end
-
-  # def guest_login
-  #   User.find_or_create_by!(email: 'guest@example.com') do |user|
-  #     user.password = SecureRandom.urlsafe_base64
-  #   end
-
-  #   token = encode_jwt_token_for(user)
-  #   render json: { token: token }
-  # end
-
-  # def encode_jwt_token_for(user)
-  #   exp = 24.hours.from_now.to_i # 有効期限を24時間後に設定
-  #   payload = { user_id: user.id, exp: exp }
-  #   hmac_secret = ENV['JWT_SECRET_KEY']
-  #   JWT.encode(payload, hmac_secret, 'HS256')
-  # end
 end
