dfc58c6d5964e1776f3cb1280d98d7326bc1377a
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-04-04 15:39:16 +0900

    コメントアウト解除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,27 +1,27 @@
 class ApplicationController < ActionController::API
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
 
-  # private
+  private
 
-  #   def authenticate_user!
-  #     authorization_header = request.headers['Authorization']
+    def authenticate_user!
+      authorization_header = request.headers['Authorization']
 
-  #     if authorization_header.present?
-  #       token = authorization_header.split.last
-  #       payload = decode_jwt(token)
-  #       @current_user = User.find_by(id: payload['uid'])
+      if authorization_header.present?
+        token = authorization_header.split.last
+        payload = decode_jwt(token)
+        @current_user = User.find_by(id: payload['uid'])
 
-  #       unless @current_user
-  #         render json: { error: 'Not Authorized' }, status: :unauthorized
-  #       end
-  #     else
-  #       render json: { error: 'Authorization token missing' }, status: :unauthorized
-  #     end
-  #   end
+        unless @current_user
+          render json: { error: 'Not Authorized' }, status: :unauthorized
+        end
+      else
+        render json: { error: 'Authorization token missing' }, status: :unauthorized
+      end
+    end
 
-  #   def decode_jwt(token)
-  #     JWT.decode(token, ENV['NEXTAUTH_SECRET'], true, { algorithm: 'HS256' }).first
-  #   rescue JWT::DecodeError
-  #     nil
-  #   end
+    def decode_jwt(token)
+      JWT.decode(token, ENV['NEXTAUTH_SECRET'], true, { algorithm: 'HS256' }).first
+    rescue JWT::DecodeError
+      nil
+    end
 end
