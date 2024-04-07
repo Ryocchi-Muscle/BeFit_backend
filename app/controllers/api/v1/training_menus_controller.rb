7f6154b6642e1a1eb554914e8e7ea3de9148dcb5
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-04-07 18:00:12 +0900

    create training_menu_controller

--- app/controllers/api/v1/training_menus_controller.rb
+++ app/controllers/api/v1/training_menus_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::TrainingMenusController < ApplicationController
+  def create; end
+
+  private
+
+    def set_training_day
+      @training_day = TrainingDay.find(params[:training_day_id])
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: 'Training day not found' }, status: :not_found
+    end
+
+    def training_menu_params
+      params.require(:training_mueu).permit(
+        :body_part,
+        :exercise_name,
+        sets_attributes: [
+          :weight,
+          :reps,
+          :completed
+        ]
+      )
+    end
 end
