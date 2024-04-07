e52b41a7600508e914e5f5debf5d70cd301f74ce
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-04-07 16:28:29 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   post 'auth/:provider/callback', to: 'api/v1/users#create'
   namespace :api do
     namespace :v1 do
-      resources :training_days do
+      resources :training_days, only: [:show] do
         resources :training_records
       end
       delete '/users/:uid', to: 'users#destroy'
