a0cbd153afa52575530f119f37bb8209d8ff910d
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-04-10 20:12:27 +0900

    add change route

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   post 'auth/:provider/callback', to: 'api/v1/users#create'
   namespace :api do
     namespace :v1 do
-      resources :training_days, only: [:show] do
-        resources :training_records
+      resources :training_days do
+        resources :training_menus do
+          resources :sets
+        end
       end
       delete '/users/:uid', to: 'users#destroy'
     end
