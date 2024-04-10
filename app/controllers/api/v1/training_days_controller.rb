34672eb7751079a686c660ee7b1c62860814e9de
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-04-10 21:16:03 +0900

    add index&show

--- app/controllers/api/v1/training_days_controller.rb
+++ app/controllers/api/v1/training_days_controller.rb
@@ -1,8 +1,10 @@
 class Api::V1::TrainingDaysController < ApplicationController
-  def show
-    training_day = TrainingDay.find(params[:id])
-    render json: training_day
-  rescue ActiveRecord::RecordNotFound
-    render json: { error: 'Training day not found' }, status: :not_found
-  end
+ def index
+  @training_days = current_user.training_days
+  render json: @training_days
+ end
+
+ def show
+  @training_days = current_user.training_days
+ end
 end
