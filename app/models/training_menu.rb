6de22760503df325314e0e5d4c995b700463ed58
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-04-11 21:49:37 +0900

    add accepts_nested_attributes_for

--- app/models/training_menu.rb
+++ app/models/training_menu.rb
@@ -1,7 +1,7 @@
 class TrainingMenu < ApplicationRecord
   belongs_to :training_day
   has_many :training_sets, dependent: :destroy
-  # accepts_nested_attributes_for :training_sets
-  validates :boday_part, presence: true
+  accepts_nested_attributes_for :training_sets
+  validates :body_part, presence: true
   validates :exercise_name, presence: true
 end
