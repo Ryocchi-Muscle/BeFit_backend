28cb49aa67379a3a2f08b7341bdd12a2e5e18673
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-04-11 21:44:35 +0900

    create controller

--- app/controllers/api/v1/training_menus_controller.rb
+++ app/controllers/api/v1/training_menus_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::TrainingMenusController < ApplicationController
+
+  # 特定のトレーニング日に新しいトレーニングメニューを追加
   def create
     training_day = TrainingDay.find(params[:training_day_id])
     training_menu = training_day.training_menus.new(training_menu_params)
@@ -10,16 +12,40 @@ class Api::V1::TrainingMenusController < ApplicationController
     end
   end
 
-  private
+  # 指定されたトレーニングメニューの情報を更新
+  def update
+    training_menu = TrainingMenu.find(params[:id])
 
-    def set_training_day
-      @training_day = TrainingDay.find(params[:training_day_id])
-    rescue ActiveRecord::RecordNotFound
-      render json: { error: 'Training day not found' }, status: :not_found
+    if training_menu.update(training_menu_params)
+      render json: training_menu
+    else
+      render json: training_menu.errors, status: :unprocessable_entity
     end
+  end
 
+  # 指定されたトレーニングメニューを削除
+  def destroy
+    training_menu = TrainingMenu.find(params[:id])
+    training_menu.destroy
+    head :no_content
+  end
+
+  # 特定のトレーニング日に紐づく全てのトレーニングメニューを一覧表示
+  def index
+    training_day = TrainingDay.find(params[:training_day_id])
+    training_menus = training_day.training_menus
+    render json: training_menus
+  end
+
+  # 特定のトレーニングメニューの詳細情報を表示
+  def show
+    training_menu = TrainingMenu.find(params[:id])
+    render json: training_menu
+  end
+
+  private
     def training_menu_params
-      params.require(:training_mueu).permit(
+      params.require(:training_menu).permit(
         :body_part,
         :exercise_name,
         sets_attributes: [
