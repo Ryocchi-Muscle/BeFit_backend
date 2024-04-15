8b117eb6327d405e038bdc15af7a7cdd93056011
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-04-16 06:38:06 +0900

    add config.autoload_paths += %W(#{config.root}/lib)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,32 +1,17 @@
 class ApplicationController < ActionController::API
   before_action :authenticate_request
+  attr_reader :current_user
 
   private
 
     def authenticate_request
-      authorization_header = request.headers['Authorization']
-pp request.headers['Authorization']
-      if authorization_header.present?
-        Rails.logger.info("authorization_header: #{authorization_header}")
-        token = authorization_header.split.last
-        payload = decode_jwt(token)
-         Rails.logger.info("payload: #{payload}")
-        @current_user = User.find_by(id: payload['uid'])
-        Rails.logger.info("current_user: #{@current_user}")
-
-        unless @current_user
-          render json: { error: 'Not Authorized' }, status: :unauthorized
-        end
-      else
-        render json: { error: 'Authorization token missing' }, status: :unauthorized
-      end
-    end
-
-    def decode_jwt(token)
-      decoded =  JWT.decode(token, ENV['NEXTAUTH_SECRET'], true, { algorithm: 'HS256' }).first
-      Rails.logger.debug "Decoded JWT: #{decoded}"
-    rescue JWT::DecodeError => e
-      Rails.logger.error "JWT Decode Error: #{e.message}"
-      nil
+      token = request.headers['Authorization']&.split(' ')&.last
+      Rails.logger.info("authorization_header: #{token}")
+      decoded_data = JsonWebToken.decode(token)
+      Rails.logger.info("decoded_data: #{decoded_data}")
+      @current_user = User.find_by(id: decoded_data[:user_id]) if decoded_data
+      Rails.logger.info("current_user: #{@current_user}")
+    rescue JWT::DecodeError
+      render json: { error: 'Invalid token' }, status: :unauthorized
     end
 end
