cca9c76aa8ab15245c2339553d2274608ec5efa4
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-04-20 17:19:48 +0900

    commentout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,17 @@
 class ApplicationController < ActionController::API
-  before_action :authenticate_request
-  attr_reader :current_user
+  # before_action :authenticate_request
+  # attr_reader :current_user
 
-  private
+  # private
 
-    def authenticate_request
-      token = request.headers['Authorization']&.split(' ')&.last
-      Rails.logger.info("authorization_header: #{token}")
-      decoded_data = JsonWebToken.decode(token)
-      Rails.logger.info("decoded_data: #{decoded_data}")
-      @current_user = User.find_by(id: decoded_data[:user_id]) if decoded_data
-      Rails.logger.info("current_user: #{@current_user}")
-    rescue JWT::DecodeError
-      render json: { error: 'Invalid token' }, status: :unauthorized
-    end
+  #   def authenticate_request
+  #     token = request.headers['Authorization']&.split(' ')&.last
+  #     Rails.logger.info("authorization_header: #{token}")
+  #     decoded_data = JsonWebToken.decode(token)
+  #     Rails.logger.info("decoded_data: #{decoded_data}")
+  #     @current_user = User.find_by(id: decoded_data[:user_id]) if decoded_data
+  #     Rails.logger.info("current_user: #{@current_user}")
+  #   rescue JWT::DecodeError
+  #     render json: { error: 'Invalid token' }, status: :unauthorized
+  #   end
 end
