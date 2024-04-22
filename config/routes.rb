21381f72bc78a48d0870996a089157169364e4e4
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-04-22 11:21:09 +0900

    add training_sessions

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   get 'training_sessions/update'
   post 'auth/:provider/callback', to: 'api/v1/users#create'
   namespace :api do
-    namespace :v1 do
+    namespace :v1 do # config/routes.rb
+      resources :training_sessions, only: [:show, :create, :update]
+      
       resources :training_days do
         resources :training_menus do
           resources :training_sets
