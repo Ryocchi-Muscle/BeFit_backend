cb1650fd3100749214298ccccddcb51bb16f1dce
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-04-23 20:12:52 +0900

    fix

--- app/models/training_session.rb
+++ app/models/training_session.rb
@@ -3,7 +3,7 @@ class TrainingSession < ApplicationRecord
   validates :start_date, presence: true
 
   # トレーニングセッションが開始してから経過した日数を計算するメソッド
-  def elapsed_days
+  def calculate_days
     today = Date.today
     if start_date.future?
       { remaining_days: (start_date - today).to_i }
