0732d15baa73ea8b1151d53b2b57b9434cac18a2
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-04-24 06:31:31 +0900

    fix

--- app/controllers/api/v1/training_days_controller.rb
+++ app/controllers/api/v1/training_days_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::TrainingDaysController < ApplicationController
-  before_action :set_current_user
+  before_action :set_current_user, :set_training_day, only: [:show, :update, :destroy]
 
   def index
     @training_days = current_user.training_days
@@ -7,7 +7,6 @@ class Api::V1::TrainingDaysController < ApplicationController
   end
 
   def show
-    @training_days = current_user.training_days
     render json: @training_days
   end
 
@@ -21,9 +20,26 @@ class Api::V1::TrainingDaysController < ApplicationController
     end
   end
 
+  def update
+    if @training_day.update(training_day_params)
+      render json: @training_day
+    else
+      render json: @training_day.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @training_day.destroy
+    head :no_content
+  end
+
   private
 
+    def set_current_user
+      @training_day = current_user.training_days.find(params[:id])
+    end
+
     def training_day_params
-      params.require(:training_day).permit(:date, menus: [:bodyPart, :exerciseName, { sets: [:setNumber, :weight, :reps, :completed] }])
+      params.require(:training_day).permit(:date)
     end
 end
