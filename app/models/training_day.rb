d549fd89bbe2a145c54553a0b9cf05699bc18d0e
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-04-29 13:32:44 +0900

    ?

--- app/models/training_day.rb
+++ app/models/training_day.rb
@@ -1,5 +1,6 @@
 class TrainingDay < ApplicationRecord
   belongs_to :user
   has_many :training_menus, dependent: :destroy
+  accepts_nested_attributes_for :training_menus
   validates :date, presence: true, uniqueness: { scope: :user_id }
 end
