be81a887773d3f7ab41117a4bcdcd663b9e40ce4
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-05-03 17:54:19 +0900

    add get 'api/v2/training_records'

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   post 'auth/:provider/callback', to: 'api/v1/users#create'
   delete '/users/:uid', to: 'users#destroy'
+  get 'api/v2/training_records', to:  'api/v2/training_record#index'
   namespace :api do
     namespace :v2 do
       resources :training_records, only: [:create]
