25a674a30ef397c78aa25e2545eab3a8ac72ea9b
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-05-03 15:27:02 +0900

    current_userからuser_idを作成し、設定

--- app/controllers/api/v2/training_records_controller.rb
+++ app/controllers/api/v2/training_records_controller.rb
@@ -1,22 +1,32 @@
 # app/controllers/api/v2/training_records_controller.rb
 class Api::V2::TrainingRecordsController < ApplicationController
+  before_action :set_current_user
+
   def create
+    Rails.logger.debug("Received params: #{params.inspect}")
     ActiveRecord::Base.transaction do
       training_day = TrainingDay.find_or_initialize_by(date: params[:date])
-      Rails.logger.debug("training_day: #{training_day.inspect}")
-      params[:menus].each do |menu|
-        Rails.logger.debug("menu: #{menu.inspect}")
-        training_menu = training_day.training_menus.find_or_initialize_by(exercise_name: menu[:menuName])
-        Rails.logger.debug("training_menu: #{training_menu.inspect}")
-        menu[:sets].each do |set|
-          training_set = training_menu.training_sets.find_or_initialize_by(set_number: set[:setId])
-          training_set.update(weight: set[:weight], reps: set[:reps], completed: set[:completed])
-        end
-        training_menu.save!
+
+      if current_user
+        training_day.user_id = current_user.id
       end
-      training_day.save!
+       Rails.logger.debug("training_day13: #{training_day.inspect}")
+
+      if training_day.save
+        Rails.logger.debug("training_day: #{training_day.inspect}")
+        params[:menus].each do |menu|
+          Rails.logger.debug("menu: #{menu.inspect}")
+          training_menu = training_day.training_menus.find_or_initialize_by(exercise_name: menu[:menuName])
+          Rails.logger.debug("training_menu: #{training_menu.inspect}")
+          menu[:sets].each do |set|
+            training_set = training_menu.training_sets.find_or_initialize_by(set_number: set[:setId])
+            training_set.update(weight: set[:weight], reps: set[:reps], completed: set[:completed])
+          end
+          training_menu.save!
+        end
+        render json: { status: 'success' }
+      end
     end
-    render json: { status: 'success' }
   rescue StandardError => e
     render json: { status: 'error', message: e.message }
   end
@@ -27,5 +37,3 @@ class Api::V2::TrainingRecordsController < ApplicationController
       params.require(:training_day).permit(:date, menus: [:menuId, :munuName, { sets: [:setId, :weight, :reps, :completed] }])
     end
 end
-
-# # current_user.training_sessions.new!(start_date: Date.today)
