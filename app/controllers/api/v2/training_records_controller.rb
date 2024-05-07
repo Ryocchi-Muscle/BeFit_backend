c0d12701b6559cdc587e3fb28e357afba71a9bfc
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-05-07 20:14:43 +0900

    create def weekly_summary

--- app/controllers/api/v2/training_records_controller.rb
+++ app/controllers/api/v2/training_records_controller.rb
@@ -16,42 +16,6 @@ class Api::V2::TrainingRecordsController < ApplicationController
     end
   end
 
-  # def create
-  #   Rails.logger.debug("Received params: #{params.inspect}")
-  #   ActiveRecord::Base.transaction do
-  #     training_day = TrainingDay.find_or_initialize_by(date: params[:date])
-
-  #     begin
-  #       Rails.logger.debug("training_day: #{training_day.inspect}")
-  #       training_day.user_id = current_user.id if current_user
-  #       training_day.save!
-  #     rescue StandardError => e
-  #       Rails.logger.error("An error occurred: #{e.message}")
-  #       render json: { status: 'error', message: e.message }, status: :internal_server_error # 500
-  #     end
-
-  #     params[:menus].each do |menu|
-  #       training_menu = training_day.training_menus.find_or_initialize_by(exercise_name: menu[:menuName])
-  #       Rails.logger.debug("training_day: #{training_day.inspect}")
-  #       menu[:sets].each do |set|
-  #         training_set = training_menu.training_sets.find_or_initialize_by(set_number: set[:setId])
-  #         training_set.update(weight: set[:weight], reps: set[:reps], completed: set[:completed])
-  #         Rails.logger.debug("training_set: #{training_set.inspect}")
-  #         Rails.logger.debug("training_set.attributes: #{training_set.attributes}")
-  #         training_set.save!
-  #       end
-  #       Rails.logger.debug("training_menu: #{training_menu.inspect}")
-  #       training_menu.save!
-  #     end
-
-  #     render json: { status: 'success' }
-  #   rescue ActiveRecord::RecordInvalid => e
-  #     render json: { status: 'error', errors: e.record.errors.full_messages }, status: :unprocessable_entity # 422
-  #     raise ActiveRecord::Rollback
-  #   end
-  # end
-
-
   def create
     ActiveRecord::Base.transaction do
       training_day = TrainingDay.find_or_initialize_by(date: params[:date], user_id: current_user.id)
@@ -60,10 +24,10 @@ class Api::V2::TrainingRecordsController < ApplicationController
       # 存在するメニューIDを取得
       existing_menus_ids = training_day.training_menus.pluck(:id)
       Rails.logger.debug("existing_menus_ids: #{existing_menus_ids}")
-      client_menu_ids = params[:menus].map{ |menu| menu[:menuId] }
+      client_menu_ids = params[:menus].map { |menu| menu[:menuId] }
       Rails.logger.debug("client_menu_ids: #{client_menu_ids}")
 
-      training_day.training_menus.where(id:existing_menus_ids - client_menu_ids).destroy_all
+      training_day.training_menus.where(id: existing_menus_ids - client_menu_ids).destroy_all
 
       params[:menus].each do |menu|
         training_menu = training_day.training_menus.find_or_initialize_by(exercise_name: menu[:menuName])
@@ -72,12 +36,12 @@ class Api::V2::TrainingRecordsController < ApplicationController
 
         # 存在するセットIDを取得
         existing_sets = training_menu.training_sets.pluck(:id)
-        client_set_ids = menu[:sets].map{ |set| set[:setId] }
+        client_set_ids = menu[:sets].map { |set| set[:setId] }
 
         training_menu.training_sets.where(id: existing_sets - client_set_ids).destroy_all
 
         menu[:sets].each do |set|
-          training_set = training_menu.training_sets.find_or_initialize_by(set_number: set[:setNumber])
+          training_set = training_menu.training_sets.find_or_initialize_by(id: set[:setId])
           training_set.update(set_number: set[:setNumber], weight: set[:weight], reps: set[:reps], completed: set[:completed])
           training_set.save!
         end
@@ -90,6 +54,18 @@ class Api::V2::TrainingRecordsController < ApplicationController
       raise ActiveRecord::Rollback
     end
   end
+
+  def weekly_summary
+    start_date = params[:date].to_date.beginning_of_week(:sunday)
+    end_date = start_date.end_of_week(:sunday)
+
+    current_week_weight = TrainingDay.where(date: start_date...end_date).joins(training_menus: :training_sets).sum(
+      'training_sets.weight * training_sets.reps'
+    )
+
+    render json: { week_start: start_date, total_weight: current_week_weight }
+  end
+
   private
 
     def training_day_params
