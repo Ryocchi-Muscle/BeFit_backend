6d34bf79b735b476737ad377e91db1c05f6e4089
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-05-07 21:30:16 +0900

    add :weekly_summary for resources :training_records

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'api/v2/training_records', to: 'api/v2/training_record#index'
   namespace :api do
     namespace :v2 do
-      resources :training_records, only: [:show, :create]
+      resources :training_records, only: [:show, :create, :weekly_summary]
     end
   end
 end
