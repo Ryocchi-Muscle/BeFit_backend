08d153a34e330a36824c2c0a5d4d28af0d788142
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-05-19 12:37:37 +0900

    add program menu in app/controllers/api/v2/personalized_menus_controller.rb

--- app/controllers/api/v2/personalized_menus_controller.rb
+++ app/controllers/api/v2/personalized_menus_controller.rb
@@ -11,26 +11,61 @@ class Api::V2::PersonalizedMenusController < ApplicationController
 
   private
 
-    def generate_program(gender, frequency)
-      case gender
-      when 'male'
-        case frequency
-        when '1'
-          %w[プログラムA1 プログラムA2]
-        when '2'
-          %w[プログラムB1 プログラムB2]
-        when '3'
-          %w[プログラムC1 プログラムC2]
-        end
-      when 'female'
-        case frequency
-        when '1'
-          %w[プログラムD1 プログラムD2]
-        when '2'
-          %w[プログラムE1 プログラムE2]
-        when '3'
-          %w[プログラムF1 プログラムF2]
-        end
+  def generate_program(gender, frequency)
+    base_url = request.base_url
+     Rails.logger.debug "Base URL: #{base_url}"
+    base_url = request.base_url
+    case gender
+    when 'male'
+      case frequency
+      when '1'
+          [
+          {
+            title: "週に1回のプログラム",
+            image: "#{base_url}/images/images.jpeg",
+            details: [
+              "スクワット: 10回3セット, インターバル3分",
+              "スクワット: メインセットから重量を15%落として限界まで1セット, インターバル1分",
+              "ベンチプレス: 10回3セット, インターバル3分 (時間なければ2分)",
+              "ベンチプレス: メインセットから重量を15%落として限界まで1セット, インターバル1分",
+              "デッドリフト: 10回2セット, インターバル3分",
+              "懸垂: インターバル3分",
+              "サイドレイズ: 12回3セット, インターバル1分",
+              "アブローラー: 2セット, インターバル2分",
+              "※はじめの15%落として限界まで行うセットはこのプログラムを始めて1ヶ月以上経過してから取り入れてください。それまでは10回3セットだけでOKです。"
+            ]
+          }
+        ]
+      when '2-3'
+                  [
+          {
+            title: "週に2-3回のプログラム",
+            image: "#{base_url}/images/images.jpeg",
+            details: [
+              "スクワット: 10回3セット, インターバル3分",
+              "スクワット: メインセットから重量を15%落として限界まで1セット, インターバル1分",
+              "ベンチプレス: 10回3セット, インターバル3分 (時間なければ2分)",
+              "ベンチプレス: メインセットから重量を15%落として限界まで1セット, インターバル1分",
+              "デッドリフト: 10回2セット, インターバル3分",
+              "懸垂: インターバル3分",
+              "サイドレイズ: 12回3セット, インターバル1分",
+              "アブローラー: 2セット, インターバル2分",
+              "※はじめの15%落として限界まで行うセットはこのプログラムを始めて1ヶ月以上経過してから取り入れてください。それまでは10回3セットだけでOKです。"
+            ]
+          }
+        ]
+      when '4-6'
+        ["プログラムC1", "プログラムC2"]
+      end
+    when 'female'
+      case frequency
+      when '1'
+        ["プログラムD1", "プログラムD2"]
+      when '2-3'
+        ["プログラムE1", "プログラムE2"]
+      when '4-6'
+        ["プログラムF1", "プログラムF2"]
       end
     end
+  end
 end
