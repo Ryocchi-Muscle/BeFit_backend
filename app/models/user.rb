6b51535c5f5975437c02a2a440ddf4ad2348966b
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-06-01 21:33:50 +0900

    add  has_one :program, dependent: :destoroy in User's model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_many :training_days, dependent: :destroy
   has_many :training_sessions, dependent: :destroy
   has_many :training_programs, dependent: :destroy
+  has_one :program, dependent: :destoroy
 end
