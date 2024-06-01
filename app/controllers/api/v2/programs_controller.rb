b23ef5d715182305096fb59bfea67ec798193daa
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-06-01 22:22:20 +0900

    ProgramsController

--- app/controllers/api/v2/programs_controller.rb
+++ app/controllers/api/v2/programs_controller.rb
@@ -2,41 +2,36 @@ class Api::V2::ProgramsController < ApplicationController
   before_action :set_current_user
 
   def index
-    programs = Program.where(user: @current_user)
-    render json: { program: programs}
+    program = Program.where(user: @current_user)
+    render json: { program: program}
   end
 
   def create
     Rails.logger.debug "Program Params: #{params[:program].inspect}"
 
-    if @current_user.programs.exists?
+    if @current_user.programs.present?
       render json: { success: false, errors: ["Program already exists"] }, status: :unprocessable_entity
       return
     end
 
-    program_params = params.require(:program).map do |p|
-      ActionController::Parameters.new(p.to_unsafe_h).permit(:week, details: [:menu, :set_info, :other])
-    end
+    program_params = params.require(:program).permit(:week,details: [:menu, :set_info, :other])
 
-    programs = program_params.map do |program_params|
       program = Program.new(program_params)
       program.user = @current_user
-      program.save
-      program
-    end
 
-
-    if programs.all?(&:persisted?)
-      render json: { success: true, programs: programs }, status: :created
-    else
-      render json: { success: false, errors: programs.map(&:errors).map(&:full_messages) }, status: :unprocessable_entity
-    end
+      if program.after_save
+        render json: { sucess: true, progmram: program}, status: :created
+      else
+        render json: { success: false, errors: program.errors.full_messages }, status: :unprocessable_entity
+      end
   end
 
   def destroy
-    program = @current_user.programs.find(params[:id])
-    if program.destroy
-     render json: { success: true, message: "deleted plans" }, status: ok
+    program = @current_user.program
+    if program.nil?
+      render json: { success: false, errors: ["Program not found"] }, status: :not_found
+    elsif program.destroy
+     render json: { success: true, message: "deleted plans" }, status: :ok
     else
       render json: { success: false, errors: program.errors.full_messages }, status: :unprocessable_entity
     end
