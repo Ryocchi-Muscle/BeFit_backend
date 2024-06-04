e91abee281c26d0d396c93e4f6b0172f06a3932e
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-06-04 13:05:11 +0900

    Fix migration to check for foreign key existence

--- db/migrate/20240602020201_remove_user_id_from_daily_programs.rb
+++ db/migrate/20240602020201_remove_user_id_from_daily_programs.rb
@@ -1,6 +1,10 @@
 class RemoveUserIdFromDailyPrograms < ActiveRecord::Migration[7.0]
   def change
-    remove_foreign_key :daily_programs, :users
-    remove_column :daily_programs, :user_id
+    # 外部キーが存在する場合のみ削除
+    if foreign_key_exists?(:daily_programs, :users)
+      remove_foreign_key :daily_programs, :users
+    end
+
+    remove_column :daily_programs, :user_id, :integer
   end
 end
