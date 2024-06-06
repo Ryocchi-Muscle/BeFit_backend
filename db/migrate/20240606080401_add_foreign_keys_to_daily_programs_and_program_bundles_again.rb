623ab4acb56431f5ef940377f647582dade2d5e5
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-06-06 17:11:08 +0900

    add_foreign_keys_to_daily_programs_and_program_bundles_again

--- db/migrate/20240606080401_add_foreign_keys_to_daily_programs_and_program_bundles_again.rb
+++ db/migrate/20240606080401_add_foreign_keys_to_daily_programs_and_program_bundles_again.rb
@@ -1,4 +1,4 @@
-class AddForeignKeysToDailyProgramsAndProgramBundles2 < ActiveRecord::Migration[7.0]
+class AddForeignKeysToDailyProgramsAndProgramBundlesAgain < ActiveRecord::Migration[7.0]
   def change
     add_foreign_key :daily_programs, :program_bundles, column: :program_bundle_id, name: 'fk_daily_programs_program_bundles_custom', on_delete: :cascade
     add_foreign_key :program_bundles, :users, column: :user_id, name: 'fk_program_bundles_users_custom', on_delete: :cascade
