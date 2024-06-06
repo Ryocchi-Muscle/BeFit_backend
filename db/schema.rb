6f0c74449cc32d3fb980be1bc8818e31bb03bbc7
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-06-06 19:55:44 +0900

    drop_table :todos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_06_085628) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_06_105418) do
   create_table "daily_programs", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.json "details"
     t.datetime "created_at", null: false
@@ -30,13 +30,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_06_085628) do
     t.integer "duration", null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "fk_program_bundles_users_custom"
-  end
-
-  create_table "todos", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "title"
-    t.boolean "completed"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "training_days", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
