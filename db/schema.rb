bc18ea1718891a173551ddd2eeeeb94f77960932
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-06-08 16:52:32 +0900

    fix personalized_menus_controller & add_foreign_key daily_programs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_06_123708) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_08_070514) do
   create_table "daily_programs", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.json "details"
     t.datetime "created_at", null: false
@@ -19,7 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_06_123708) do
     t.integer "week", null: false
     t.boolean "completed", default: false, null: false
     t.integer "day"
-    t.index ["program_bundle_id"], name: "index_daily_programs_on_program_bundle_id", unique: true
+    t.index ["program_bundle_id", "week", "day"], name: "index_daily_programs_on_program_bundle_id_and_week_and_day", unique: true
   end
 
   create_table "program_bundles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
