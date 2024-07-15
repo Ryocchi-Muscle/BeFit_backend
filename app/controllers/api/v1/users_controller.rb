4fb4ff58f9a8b0c9b804d2fbc916775f533d07f4
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-07-15 14:57:48 +0900

    fix routes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :set_current_user!, only: [:destroy]
   def create
     # 引数の条件に該当するデータがあればそれを返す。なければ新規作成する
-    user = User.find_or_create_by(provider: params[:provider], uid: params[:uid], name: params[:name])
+    user = User.find_or_create_by(user_params)
     if user
       head :ok
     else
@@ -15,10 +16,17 @@ class Api::V1::UsersController < ApplicationController
     user = User.find_by(uid: params[:uid])
     if user
       user.destroy
+      head :no_content
     else
       render json: { error: "ユーザーが見つかりません" }, status: :not_found
     end
   rescue StandardError => e
     render json: { error: e.message }, status: :internal_server_error
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:provider, :uid, :name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   post 'auth/:provider/callback', to: 'api/v1/users#create'
-  delete '/users/:uid', to: 'users#destroy'
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:destroy], params: :uid
+    end
+  end
+
   namespace :api do
     namespace :v2 do
       post 'personalized_menus/create_and_save', to: 'personalized_menus#create_and_save'
-      get 'personalized_menus', to: 'personalized_menus#index'
-      delete 'personalized_menus/:id', to: 'personalized_menus#destroy'
-      patch 'personalized_menus/:id/update', to: 'personalized_menus#update'
-
+      resources :personalized_menus, only: [:index,:destroy, :update]
       resources :training_records, only: [:index, :show, :update] do
         get 'weekly_summary', on: :collection
         get 'check_completion/:date/:program_id', to: 'training_records#check_completion', on: :collection
