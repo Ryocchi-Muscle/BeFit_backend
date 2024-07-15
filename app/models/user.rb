5e792f9894ccddb3174428845f4a4c0756478122
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-07-15 22:23:31 +0900

    add user's destroy func

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_many :training_days, dependent: :destroy
-  has_many :training_sessions, dependent: :destroy
+  # has_many :training_sessions, dependent: :destroy
   has_one :program_bundle, dependent: :destroy
 end
