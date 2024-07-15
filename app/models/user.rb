ad5ec9a7660773fa140fe8a025ebd82fec19c1bd
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-07-15 22:29:50 +0900

    delete has_many session

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
   has_many :training_days, dependent: :destroy
-  # has_many :training_sessions, dependent: :destroy
   has_one :program_bundle, dependent: :destroy
 end
