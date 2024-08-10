2b705da61af9af9a2270daebb2501895a58cd163
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-08-10 23:11:05 +0900

    fix user_model_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,31 +1,31 @@
 require 'rails_helper'
 
-Rspec.describe User, type: :model do
+RSpec.describe User, type: :model do
   describe "asociations" do
     it "has many training_days" do
      association = described_class.reflect_on_association(:training_days)
-     expect(association.marco).to eq :has_many
+     expect(association.macro).to eq :has_many
     end
 
     it "has one program_bundle" do
-      association = describe_class.reflect_on_association(:program_bundle)
-      expect(association.marco).to eq :has_one
+      association = described_class.reflect_on_association(:program_bundle)
+      expect(association.macro).to eq :has_one
     end
   end
 
   describe "dependent destroy" do
-    it "destroy associated training_days when use is destroyed" do
+    it "destroy associated training_days when user is destroyed" do
       user = User.create!
-      user.training_days.create!
+      user.training_days.create!(date: Date.today)
 
       expect { user.destroy }.to change { TrainingDay.count }.by(-1)
     end
 
     it "destroy associated program_bundle when user is destroyed" do
       user = User.create!
-      user.create_program_bundle!
+      user.create_program_bundle!(gender: "male", frequency: 3, duration: 12)
 
-      expect { user.destroy }.to chenge { ProgramBUndle.count }.by(-1)
+      expect { user.destroy }.to change { ProgramBundle.count }.by(-1)
     end
   end
 end
