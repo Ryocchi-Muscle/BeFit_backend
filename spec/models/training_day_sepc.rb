c85f9bf6739e17401ed2f9f64a387c147158a32c
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-08-13 23:58:31 +0900

    関連付けのテスト

--- spec/models/training_day_sepc.rb
+++ spec/models/training_day_sepc.rb
@@ -20,4 +20,16 @@ RSpec.describe TrainingDay, type: :model do
     expect(duplicate_training_day).not_to be_valid
     expect(duplicate_training_day.errors[:date]).to include("has already been taken")
   end
+
+    it 'ユーザーと関連付けられていること' do
+    training_day = TrainingDay.new(date: Date.today, user: user)
+    expect(training_day.user).to eq(user)
+  end
+
+  it 'training_menusが関連付けられており、dependent: :destroyが正しく動作すること' do
+    training_day = TrainingDay.create!(date: Date.today, user: user)
+    training_day.training_menus.create!(name: 'Squat', sets: 3)
+
+    expect { training_day.destroy }.to change { TrainingMenu.count }.by(-1)
+  end
 end
