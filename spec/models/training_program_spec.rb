acbe0d37586ca0ed4951498ddcf1ccec6a834438
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-08-26 09:12:20 +0900

    describe 'スコープテスト' do

--- spec/models/training_program_spec.rb
+++ spec/models/training_program_spec.rb
@@ -29,4 +29,30 @@ RSpec.describe TrainingProgram, type: :model do
     end
   end
 
+    describe 'スコープテスト' do
+    let(:male_program) { create(:training_program, gender: 'male', user: user) }
+    let(:female_program) { create(:training_program, gender: 'female', user: user) }
+
+    it 'by_genderスコープが正しく動作すること' do
+      expect(TrainingProgram.by_gender('male')).to include(male_program)
+      expect(TrainingProgram.by_gender('male')).not_to include(female_program)
+    end
+
+    let(:high_frequency_program) { create(:training_program, frequency: '5 times a week', user: user) }
+    let(:low_frequency_program) { create(:training_program, frequency: '2 times a week', user: user) }
+
+    it 'by_frequencyスコープが正しく動作すること' do
+      expect(TrainingProgram.by_frequency('5 times a week')).to include(high_frequency_program)
+      expect(TrainingProgram.by_frequency('5 times a week')).not_to include(low_frequency_program)
+    end
+
+    let(:short_duration_program) { create(:training_program, week: 4, user: user) }
+    let(:long_duration_program) { create(:training_program, week: 12, user: user) }
+
+    it 'by_durationスコープが正しく動作すること' do
+      expect(TrainingProgram.by_duration(4)).to include(short_duration_program)
+      expect(TrainingProgram.by_duration(4)).not_to include(long_duration_program)
+    end
+  end
+
 end
