6ef51bc79e337772a125f83ca3f43356eff3cae6
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-09-21 17:13:51 +0900

    delete app/controllers/api/v1/training_records_controller.rb

--- app/controllers/api/v1/training_records_controller.rb
+++
@@ -1,8 +0,0 @@
-class Api::V1::TrainingRecordsController < ApplicationController
-  def create
-    result = TrainingRecord.Service.create_record(params)
-    render json: result, status: :created
-  rescue ActiveRecord::RecordInvalid => e
-    render json: { error: e.record.errors.full_messgaes }, status: :unprocessable_entity
-  end
-end
